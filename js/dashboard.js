7466a13bea4b240b4effdd4ac43b79d5eeea1443
Shailendra Chauhan <pro.shailendra@gmail.com>
2015-07-02 21:32:18 +0530

    Ionic App
    Ionic App

---
+++ js/dashboard.js
@@ -0,0 +1,35 @@
+﻿app.controller("dashboardCtrl", function ($scope, customerService, $state) {
+    $scope.customers = [];
+
+    //get all Customers
+    $scope.getAll = function () {
+        customerService.GetCustomers().success(function (data) {
+            console.log(data);
+            $scope.customers = data;
+        }).error(function (data) {
+            $scope.error = "An Error has occured while Loading customers! " + data.ExceptionMessage;
+        });
+    };
+
+    // add Customer
+    $scope.add = function (customer) {
+
+        if (customer != null && customer.name != null && customer.address && customer.contactno) {
+            customerService.AddCustomer(customer).success(function (data) {
+                //reset form
+                $scope.customer = null;
+                // go to view
+                $state.go('dashboard.home', { reload: true });
+            }).error(function (data) {
+                $scope.error = "An Error has occured while Adding customer! " + data.ExceptionMessage;
+            });
+        }
+    };
+
+    $scope.cancel = function () {
+        // go to view
+        $state.go('dashboard.home');
+    };
+
+    $scope.getAll();
+});
---
+++ js/service.js
@@ -0,0 +1,41 @@
+﻿//app.constant("baseAddress", "http://localhost:56853/");
+app.constant("baseAddress", "http://app.dotnettricks.io/");
+
+app.service("userService", function ($http, baseAddress) {
+    return {
+        loginUser: function (user) {
+            var data = { Username: user.username, Password: user.password };
+            serviceUrl = baseAddress + "UserService/LoginUser";
+            return $http.post(serviceUrl, data);
+        },
+        SignupUser: function (user) {
+            serviceUrl = baseAddress + "UserService/UserSignUp";
+            return $http.post(serviceUrl, user);
+        }
+    }
+});
+
+app.service("customerService", function ($http, baseAddress) {
+    return {
+        GetCustomers: function () {
+            serviceUrl = baseAddress + "CustomerService/GetCustomers";
+            return $http.get(serviceUrl);
+        },
+        GetCustomer: function (id) {
+            serviceUrl = baseAddress + "CustomerService/GetCustomer/" + id;
+            return $http.get(serviceUrl);
+        },
+        AddCustomer: function (customer) {
+            serviceUrl = baseAddress + "CustomerService/AddCustomer";
+            return $http.post(serviceUrl, customer);
+        },
+        DeleteCustomer: function (customer) {
+            serviceUrl = baseAddress + "CustomerService/DeleteCustomer/" + customer.CustomerId;
+            return $http.delete(serviceUrl);
+        },
+        UpdateCustomer: function (customer) {
+            serviceUrl = baseAddress + "CustomerService/UpdateCustomer/" + customer.CustomerId;
+            return $http.put(serviceUrl, customer);
+        }
+    };
+});
